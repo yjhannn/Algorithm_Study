af0707db9e7670832849fb9a10dca884583d5dda
yjhannn <114985804+yjhannn@users.noreply.github.com>
2025-02-19 22:17:40 +0900

    문자열 : 4949

---
+++ 백준/PROBLEM/4949.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+int main() {
+  while(1) {
+    string str;
+    stack<int> stk;
+    bool check = true;
+    getline(cin, str);
+    if(str == ".")
+      break;
+    for(int i = 0; i < str.size(); i++) {
+      if(str[i] == ')') {
+        if(stk.size() == 0 || stk.top() == '[') {
+          check = false;
+          break;
+        }
+        else
+          stk.pop();
+      }
+      else if(str[i] == ']') {
+        if(stk.size() == 0 || stk.top() == '(') {
+          check = false;
+          break;
+        }
+        else
+          stk.pop();
+      }
+      if(str[i] == '(')
+        stk.push(str[i]);
+      else if(str[i] == '[')
+        stk.push(str[i]);
+    }
+    if(check && stk.size() == 0)
+      cout << "yes\n";
+    else
+      cout << "no\n";
+  }
+
+  return 0;
+}
