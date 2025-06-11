b4faa5d086b9fabf8ed5209433b47f9e8e251577
yjhannn <114985804+yjhannn@users.noreply.github.com>
2025-06-11 15:35:38 +0900

    Two Pointer : 1644(다시 풀기)

---
+++ 백준/PROBLEM/1644.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+bool check[4000001];
+int n, ret, low, high, sum, p, a[2000001];
+int main() {
+  cin >> n;
+  // 소수 구하기
+  for(int i = 2; i <= n; i++) {
+    if(check[i])
+      continue;
+      for(int j = 2*i; j <= n; j+=i) {
+        check[j] = 1;
+      }
+  }
+  for(int i = 2; i <= n; i++) {
+    if(!check[i])
+      a[p++] = i;
+  }
+
+  while(1) {
+    if(sum >= n)
+      sum -= a[low++];
+    else if(high == p)
+      break;
+    else
+      sum += a[high++];
+    if(sum == n)
+      ret++;
+  }
+  cout << ret;
+  return 0;
+}
